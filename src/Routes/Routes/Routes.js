cde8db5d4ae0354ba9bf265186d46ac1f044a0ac
raihaanshaarif <codexbd@gmail.com>
2023-05-02 20:02:10 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,23 @@
+import Login from "../../pages/Login/Login";
+
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../../Layout/Main");
+const { default: Home } = require("../../pages/Home/Home/Home");
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+    }
+])
+export default router;
